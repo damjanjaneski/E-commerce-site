95c5a834f41a1457705fcc7c38843db1722e216f
damjanjaneski <djaneski10@gmail.com>
2023-11-22 20:46:49 +0100

    Footer added

---
+++ tummytox/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>
+            <b>TUMMY</b>TOX
+          </h2>
+        </div>
+        <div>
+          <p>About Us</p>
+          <p>Delivery & Shipping</p>
+          <p>Contact Us</p>
+        </div>
+        <div>
+          <h3>PAYMENT METHODS</h3>
+        </div>
+        <div></div>
+      </div>
+      <p>Copyright © 2023 All Rights Reserved</p>
+    </div>
+  );
+}
