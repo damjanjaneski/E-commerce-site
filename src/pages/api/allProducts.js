5ae4923cccdc8afb0f830c58f1f869c00340d721
damjanjaneski <djaneski10@gmail.com>
2024-01-22 20:28:23 +0100

    not fixed yet, trying again

--- src/pages/api/allProducts.js
+++ src/pages/api/allProducts.js
@@ -1,38 +1,75 @@
 export default async (req, res) => {
   let allProducts = [];
 
-  const bestsellers = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/bestsellers`
-  ).then((res) => res.json());
+  if (process.env.NEXT_PUBLIC_API_URL === "http://localhost:3000") {
+    const bestsellers = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/bestsellers`
+    ).then((res) => res.json());
 
-  const accessories = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=accessories`
-  ).then((res) => res.json());
+    const accessories = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=accessories`
+    ).then((res) => res.json());
 
-  const healthAndDetox = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=healthanddetox`
-  ).then((res) => res.json());
+    const healthAndDetox = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=healthanddetox`
+    ).then((res) => res.json());
 
-  const weightloss = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=weightloss`
-  ).then((res) => res.json());
+    const weightloss = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=weightloss`
+    ).then((res) => res.json());
 
-  const sport = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=sport`
-  ).then((res) => res.json());
+    const sport = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=sport`
+    ).then((res) => res.json());
 
-  const beauty = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=beauty`
-  ).then((res) => res.json());
+    const beauty = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=beauty`
+    ).then((res) => res.json());
 
-  allProducts.push(
-    ...bestsellers,
-    ...accessories,
-    ...healthAndDetox,
-    ...weightloss,
-    ...sport,
-    ...beauty
-  );
+    allProducts.push(
+      ...bestsellers,
+      ...accessories,
+      ...healthAndDetox,
+      ...weightloss,
+      ...sport,
+      ...beauty
+    );
 
-  return res.status(200).json(allProducts);
+    return res.status(200).json(allProducts);
+  } else {
+    const bestsellers = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/bestsellers`
+    ).then((res) => res);
+
+    const accessories = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=accessories`
+    ).then((res) => res);
+
+    const healthAndDetox = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=healthanddetox`
+    ).then((res) => res);
+
+    const weightloss = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=weightloss`
+    ).then((res) => res);
+
+    const sport = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=sport`
+    ).then((res) => res);
+
+    const beauty = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/productsList?collection=beauty`
+    ).then((res) => res);
+
+    allProducts.push(
+      ...bestsellers,
+      ...accessories,
+      ...healthAndDetox,
+      ...weightloss,
+      ...sport,
+      ...beauty
+    );
+
+    return allProducts;
+  }
 };
